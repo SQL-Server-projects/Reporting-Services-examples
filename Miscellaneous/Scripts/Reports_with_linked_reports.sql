e63e9edcc5ddd1d70e0b8cdaf704924a6a25c3a6
Anthony <anthonyduguid@gmail.com>
2022-07-27 20:21:39 +1000

    Update Reports_with_linked_reports.sql

--- Miscellaneous/Scripts/Reports_with_linked_reports.sql
+++ Miscellaneous/Scripts/Reports_with_linked_reports.sql
@@ -3,9 +3,9 @@
 		 [Original_Report] = [prt].[Path]
 	    , [Linked_Report] = [rpt].[Path]
     FROM
-	    [ssrs].[Catalog](NOLOCK) AS [rpt]
-	    INNER JOIN [ssrs].[Users](NOLOCK) AS [urm] ON [rpt].[ModifiedById] = [urm].[UserID]
-	    LEFT JOIN [ssrs].[Catalog](NOLOCK) AS [prt] ON [prt].[ItemID] = [rpt].[LinkSourceID] 
+	    [dbo].[Catalog](NOLOCK) AS [rpt]
+	    INNER JOIN [dbo].[Users](NOLOCK) AS [urm] ON [rpt].[ModifiedById] = [urm].[UserID]
+	    LEFT JOIN [dbo].[Catalog](NOLOCK) AS [prt] ON [prt].[ItemID] = [rpt].[LinkSourceID] 
     WHERE
 	  1 = 1
 	  AND [rpt].[Type] IN(4);
