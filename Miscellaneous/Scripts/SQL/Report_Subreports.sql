662f86d883e31873c9c524ce540c0492070e78be
Anthony <anthonyduguid@gmail.com>
2025-02-24 11:05:59 +1100

    Update Report_Subreports.sql

--- Miscellaneous/Scripts/SQL/Report_Subreports.sql
+++ Miscellaneous/Scripts/SQL/Report_Subreports.sql
@@ -1,58 +1,57 @@
-
 WITH 
-content_binaries
+[content_binaries]
 AS 
 (
     SELECT 
-	     cat.[ItemID]
-	   , cat.[Name]
-	   , cat.[Path]
-	   , cat.[Type]
+	     [cat].[ItemID]
+	   , [cat].[Name]
+	   , [cat].[Path]
+	   , [cat].[Type]
 	   , [TypeDescription] = 
-		  CASE cat.[Type]
+		  CASE [cat].[Type]
 			 WHEN 2 THEN 'Report'
 			 WHEN 5 THEN 'Data Source'
 			 WHEN 7 THEN 'Report Part'
 			 WHEN 8 THEN 'Shared Dataset'
 			 ELSE 'Other'
 		  END  
-	   , [Content] = CONVERT(VARBINARY(MAX), cat.[Content])
+	   , [Content] = CONVERT(VARBINARY(MAX), [cat].[Content])
     FROM 
-	   [dbo].[Catalog] AS cat
+	   [dbo].[Catalog] AS [cat]
     WHERE 
 	   1=1
-	   AND cat.[Content] IS NOT NULL
-	   AND cat.[Type] = 2 
+	   AND [cat].[Content] IS NOT NULL
+	   AND [cat].[Type] = 2 
 )
 ,
-content_binaries_format
+[content_binaries_format]
 AS 
 (
     SELECT 
-	     bin.[ItemID]
-	   , bin.[Name]
-	   , bin.[Path]
-	   , bin.[Type]
-	   , bin.[TypeDescription]
+	     [bin].[ItemID]
+	   , [bin].[Name]
+	   , [bin].[Path]
+	   , [bin].[Type]
+	   , [bin].[TypeDescription]
 	   , [Content] = --strip off the BOM if it exist
 		  CASE
-			 WHEN LEFT(bin.[Content], 3) = 0xEFBBBF THEN CONVERT(VARBINARY(MAX), SUBSTRING(bin.[Content], 4, LEN(bin.[Content])))
-			 ELSE bin.[Content]
+			 WHEN LEFT([bin].[Content], 3) = 0xEFBBBF THEN CONVERT(VARBINARY(MAX), SUBSTRING([bin].[Content], 4, LEN([bin].[Content])))
+			 ELSE [bin].[Content]
 		  END  
     FROM 
-	   content_binaries AS bin
+	   [content_binaries] AS [bin]
 )
 SELECT 
-	 cbf.[ItemID]
-    , cbf.[Name]
-    , cbf.[Path]
-    , cbf.[Type]
-    , cbf.[TypeDescription]
-    , cbf.[Content] 
-    , [ContentVarchar] = CONVERT(VARCHAR(MAX), cbf.[Content]) 
-    , [ContentXML] = CONVERT(XML, cbf.[Content]) 
+	 [cbf].[ItemID]
+    , [cbf].[Name]
+    , [cbf].[Path]
+    , [cbf].[Type]
+    , [cbf].[TypeDescription]
+    , [cbf].[Content] 
+    , [ContentVarchar] = CONVERT(VARCHAR(MAX), [cbf].[Content]) 
+    , [ContentXML] = CONVERT(XML, [cbf].[Content]) 
 FROM 
-    content_binaries_format AS cbf
+    [content_binaries_format] AS [cbf]
 WHERE
     1=1
-    AND CONVERT(VARCHAR(MAX), cbf.[Content]) LIKE '%<subreport%';
+    AND CONVERT(VARCHAR(MAX), [cbf].[Content]) LIKE '%<subreport%';
