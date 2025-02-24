f31b2f29c46bedf93294c9abf7671d83cfb18f8b
Anthony <anthonyduguid@gmail.com>
2025-02-24 11:03:10 +1100

    Update Report_Datasets.sql

--- Miscellaneous/Scripts/SQL/Report_Datasets.sql
+++ Miscellaneous/Scripts/SQL/Report_Datasets.sql
@@ -1,6 +1,6 @@
 
 WITH 
-catalog_xml 
+[catalog_xml] 
 AS 
 (
     SELECT 
@@ -17,7 +17,7 @@ AS
 	   [Type] = 2 
 )
 ,
-data_sources 
+[data_sources] 
 AS 
 (
     SELECT 
@@ -29,28 +29,28 @@ AS
     (
 	   SELECT 
 			[c].*
-		   , [LocalDataSourceName] = [DataSourceXml].value('@Name', 'NVARCHAR(260)')
-		   , [DataProvider] = [DataSourceXml].value('(*:ConnectionProperties/*:DataProvider)[1]', 'NVARCHAR(260)')
-		   , [ConnectionString] = [DataSourceXml].value('(*:ConnectionProperties/*:ConnectString)[1]', 'NVARCHAR(MAX)')
+		   , [LocalDataSourceName] = [DataSourceXml].[value]('@Name', 'NVARCHAR(260)')
+		   , [DataProvider] = [DataSourceXml].[value]('(*:ConnectionProperties/*:DataProvider)[1]', 'NVARCHAR(260)')
+		   , [ConnectionString] = [DataSourceXml].[value]('(*:ConnectionProperties/*:ConnectString)[1]', 'NVARCHAR(MAX)')
 	   FROM
-		   catalog_xml AS [c] 
+		   [catalog_xml] AS [c] 
 		   CROSS APPLY [ContentXml].[nodes]('/*:Report/*:DataSources/*:DataSource') AS [DataSource]([DataSourceXml])
 	   WHERE [c].[Type] = 2 -- limit to reports only
     ) AS [r]
 )
 ,
-datasets 
+[datasets] 
 AS 
 (
     SELECT 
 		 [ItemID]
-	    , [DataSetName] = [QueryXml].value('@Name', 'NVARCHAR(256)')
-	    , [DataSourceName] = [QueryXml].value('(*:Query/*:DataSourceName)[1]', 'NVARCHAR(260)')
-	    , [CommandType] = [QueryXml].value('(*:Query/*:CommandType)[1]', 'NVARCHAR(15)')
-	    , [CommandText] = [QueryXml].value('(*:Query/*:CommandText)[1]', 'NVARCHAR(MAX)')
+	    , [DataSetName] = [QueryXml].[value]('@Name', 'NVARCHAR(256)')
+	    , [DataSourceName] = [QueryXml].[value]('(*:Query/*:DataSourceName)[1]', 'NVARCHAR(260)')
+	    , [CommandType] = [QueryXml].[value]('(*:Query/*:CommandType)[1]', 'NVARCHAR(15)')
+	    , [CommandText] = [QueryXml].[value]('(*:Query/*:CommandText)[1]', 'NVARCHAR(MAX)')
 	    , [report_folder]
     FROM
-	    catalog_xml
+	    [catalog_xml]
 	    CROSS APPLY [ContentXml].[nodes]('/*:Report/*:DataSets/*:DataSet') AS [QueryData]([QueryXml])
 )
 SELECT 
@@ -61,8 +61,8 @@ SELECT
     , [CommandType] = ISNULL([CommandType], 'Text')
     , [CommandText]
 FROM
-    datasets AS [ds]
-    INNER JOIN data_sources AS [src] ON [src].[ItemID] = [ds].[ItemID] AND [src].[LocalDataSourceName] = [ds].[DataSourceName]
+    [datasets] AS [ds]
+    INNER JOIN [data_sources] AS [src] ON [src].[ItemID] = [ds].[ItemID] AND [src].[LocalDataSourceName] = [ds].[DataSourceName]
     INNER JOIN [dbo].[Catalog] AS [c] WITH(NOLOCK) ON [ds].[ItemID] = [c].[ItemID]
 WHERE
     1=1
